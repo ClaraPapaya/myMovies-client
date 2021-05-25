f5d687d74f3ad1a45a14bff4b229f615e63accae
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-05-25 16:47:07 +0200

    Added Parcel

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Objective:
 Using React, build the client-side for an application called myMovies based on its existing server-side code (REST API and database).
+
+The entry file for Parcel is index.html.
  
---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to indicate the need to bundle './index.scss'
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyMoviesApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-movies">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
+
+// Finds the root of the app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render the app in the root DOM element
+ReactDOM.render(React.createElement(MyMoviesApplication), container);
