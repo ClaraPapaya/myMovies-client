489a214cfbef8b502dd345f7fb6921343b46b084
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-06-02 16:32:09 +0200

    Update state-based routing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React from 'react';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { DirectorView } from '../director-view/directorview';
+import { GenreView } from '../genre-view/genrw/view';
 import { RegistrationView } from '../registration-view/registration-view';
 
 import './main-view.scss';
@@ -18,12 +19,21 @@ import Button from 'react-bootstrap/Button';
 export class MainView extends React.Component {
   constructor() {
     super();
+    // Inital state is set to null
     this.state = {
       movies: [],
-      // selectedMovie: null,
       user: null
-      // newUser: null
     };
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
+    }
   }
 
   getMovies(token) {
@@ -41,81 +51,91 @@ export class MainView extends React.Component {
       });
   }
 
-  componentDidMount() {
-    let accessToken = localStorage.getItem('token');
-    if (accessToken !== null) {
-      this.setState({
-        user: localStorage.getItem('user')
-      });
-      this.getMovies(accessToken);
-    }
-  }
-
-  // To connect to login-view function component
+  // To connect to login-view function component, if successful log in, it updates thes 'user' property in the state
   onLoggedIn(authData) {
     console.log(authData);
     this.setState({
       user: authData.user.Username
     });
+
     localStorage.setItem('token', authData.token);
     localStorage.setItem('user', authData.user.Username);
     this.getMovies(authData.token);
   }
 
-  // Used in render(), parts missing
-  onLoggedOut() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    this.setState({
-      user: null
-    });
-  }
+  // May be used in render(), parts missing
+  // onLoggedOut() {
+  //   localStorage.removeItem('token');
+  //   localStorage.removeItem('user');
+  //   this.setState({
+  //     user: null
+  //   });
+  // }
 
   render() {
     const { movies, user } = this.state;
-    if (!user) return
-    <Row className='justify-content-md-center'>
-      <Col md={5}>
-        <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-      </Col>
-    </Row>
-    if (movies.length === 0) return <Row className='main-view' />;
     return (
       <Router>
         <Row className='main-view justify-content-md-center'>
+
           <Route exact path='/' render={() => {
+            if (!user) return
+            <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className='main-view' />;
             return movies.map(m => (
               <Col md={3} key={m._id}>
-                <MovieCard movie={mo} />
+                <MovieCard movie={m} />
               </Col>
             ))
           }} />
+
+          <Route path='/register' render={() => {
+            if (user) return <Redirect to='/' />
+            return
+            <Col>
+              <RegistrationView />
+            </Col>
+          }} />
+
           <Route path='/movies/:movieId' render={({ match, history }) => {
+            if (!user) return
+            <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className='main-view' />;
             return
             <Col md={8}>
               <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
             </Col>
           }} />
+
           <Route path='/directors/:name' render={({ match, history }) => {
+            if (!user) return
+            <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
             if (movies.length === 0) return <div className='main-view' />;
             return <Col md={8}>
               <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
-              <Link to={`/directors/${movie.Director.Name}`}>
-                <Button variant="link">Director</Button>
-              </Link>
             </Col>
           }} />
-          <Route exact path="/genres/:name" render={({ match }) => {
+
+          <Route path='/genres/:name' render={({ match, history }) => {
+            if (!user) return
+            <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
             if (movies.length === 0) return <div className='main-view' />;
-            return <Col md={8}>
+            return
+            <Col md={8}>
               <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
-              <Link to={`/genres/${movie.Genre.Name}`}>
-                <Button variant="link">Genre</Button>
-              </Link>
             </Col>
           }} />
+
           <Button variant='dark' type='submit' onClick={() => { this.onLoggedOut() }}>Logout
-        </Button>
+          </Button>
         </Row>
       </Router>
     );
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -14,41 +14,54 @@ export function RegistrationView(props) {
 
   const handleSubmit = () => {
     e.preventDefaut();
-    console.log(username, password, email, birthday);
-    props.onRegistration(newUser);
-  };
+    // Send a request to the server to register a new user
+    axios.post('https://allmymovies.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self'); // '_self' is necessary so that the page opens in the current tab
+      })
+      .catch(e => {
+        console.log('error registering the user')
+      });
 
-  return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type='text' onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type='password' onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
+    return (
+      <Form>
+        <Form.Group>
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type='text' onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type='password' onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Email:</Form.Label>
-        <Form.Control type='text' onChange={e => setEmail(e.target.value)} />
-      </Form.Group>
+        <Form.Group>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type='text' onChange={e => setEmail(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control type='date' onChange={e => setBirthday(e.target.value)} />
-      </Form.Group>
+        <Form.Group>
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control type='date' onChange={e => setBirthday(e.target.value)} />
+        </Form.Group>
 
-      <Button variant='info' type='submit' onClick={handleSubmit}>Submit
+        <Button variant='info' type='submit' onClick={handleSubmit}>Register
       </Button>
-    </Form>
-  );
-}
+      </Form>
+    );
+  }
 
-// PropTypes to validate data type input
-RegistrationView.propTypes = {
-  username: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  birthday: PropTypes.date
-};
+  // PropTypes to validate data type input
+  RegistrationView.propTypes = {
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    birthday: PropTypes.date
+  }
+}
